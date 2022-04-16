0dadd88087dd00b786d56837a8f351c28b528f67
Pablo Baxter <pablobaxter@msn.com>
2022-04-15 23:56:40 -0700

    Update libraries, setup Hilt

--- app/src/main/java/com/frybits/android/movielist/MainActivity.kt
+++ app/src/main/java/com/frybits/android/movielist/MainActivity.kt
@@ -2,17 +2,13 @@ package com.frybits.android.movielist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.frybits.android.movielist.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/frybits/android/movielist/MovieListApp.kt
@@ -0,0 +1,7 @@
+package com.frybits.android.movielist
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieListApp : Application()
