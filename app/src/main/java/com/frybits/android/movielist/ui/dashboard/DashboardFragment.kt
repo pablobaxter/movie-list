834b10bf827c178f8f6d3b70215d1c2202f56dd9
Pablo Baxter <pablobaxter@msn.com>
2022-04-18 23:47:35 -0700

    Got movies to display by categories, but recyclerview starts at bottom

--- app/src/main/java/com/frybits/android/movielist/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/frybits/android/movielist/ui/dashboard/DashboardFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import com.frybits.android.movielist.databinding.FragmentDashboardBinding
+import com.frybits.android.movielist.ui.utils.MovieRecyclerViewAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -18,8 +20,19 @@ class DashboardFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        lifecycleScope.launch {
+        val spanCount = resources.configuration.screenWidthDp / 120
+        val adapter = MovieRecyclerViewAdapter(lifecycleScope, viewModel)
 
+        binding.mainRecyclerView.layoutManager = GridLayoutManager(context, spanCount, GridLayoutManager.VERTICAL, false).apply {
+            spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    return if (position <= 4) spanCount else 1
+                }
+            }
+        }
+        binding.mainRecyclerView.adapter = adapter
+        lifecycleScope.launch {
+            adapter.submitList(viewModel.getAllMovies())
         }
         return binding.root
     }
